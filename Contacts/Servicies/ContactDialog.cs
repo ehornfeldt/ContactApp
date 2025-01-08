e92d9f684d6bdfdf35495fc65b14921a994e53d6
ehornfeldt <nile_198907@hotmail.com>
2025-01-08 11:04:56 +0100

    Move contact dialog to own class

---
+++ Contacts/Servicies/ContactDialog.cs
@@ -0,0 +1,43 @@
+﻿using Contacts.Models;
+
+namespace Contacts.Servicies
+{
+    public class ContactDialog
+    {
+        public Contact AddDialog(Contact contact)
+        {
+
+            Console.WriteLine("Enter your first name:");
+            contact.FirstName = Console.ReadLine()!;
+            Console.WriteLine("Enter your last name:");
+            contact.LastName = Console.ReadLine()!;
+            Console.WriteLine("Enter your email:");
+            contact.Email = Console.ReadLine()!;
+            Console.WriteLine("Enter your mobile number:");
+            contact.PhoneNumber = Console.ReadLine()!;
+            Console.WriteLine("Enter your address:");
+            contact.Address = Console.ReadLine()!;
+            Console.WriteLine("Enter your zip code:");
+            contact.ZipCode = Console.ReadLine()!;
+            Console.WriteLine("Enter your city:");
+            contact.City = Console.ReadLine()!;
+
+            return contact;
+        }
+        public void ViewDialog(List<Contact> _contacts)
+        {
+
+            foreach (Contact contact in _contacts)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{"Id:",-15} {contact.Id}");
+                Console.WriteLine($"{"Name:",-15} {contact.FirstName} {contact.LastName}");
+                Console.WriteLine($"{"Email:",-15} {contact.Email}");
+                Console.WriteLine($"{"Phone number:",-15} {contact.PhoneNumber}");
+                Console.WriteLine($"{"Address:",-15} {contact.Address}, {contact.ZipCode} {contact.City}");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Contacts/Servicies/HandleContacts.cs
+++ Contacts/Servicies/HandleContacts.cs
@@ -6,27 +6,13 @@ namespace Contacts.Servicies
     public class HandleContacts
     {
         private List<Contact> _contacts = [];
+        private ContactDialog _dialog = new();
         private FileManager _fileManager = new FileManager(@"C:\projects\data", "contacts.json");
 
         public void AddContact()
         {
             Contact contact = new();
-
-            Console.WriteLine("Enter your first name:");
-            contact.FirstName = Console.ReadLine()!;
-            Console.WriteLine("Enter your last name:");
-            contact.LastName = Console.ReadLine()!;
-            Console.WriteLine("Enter your email:");
-            contact.Email = Console.ReadLine()!;
-            Console.WriteLine("Enter your mobile number:");
-            contact.PhoneNumber = Console.ReadLine()!;
-            Console.WriteLine("Enter your address:");
-            contact.Address = Console.ReadLine()!;
-            Console.WriteLine("Enter your zip code:");
-            contact.ZipCode = Console.ReadLine()!;
-            Console.WriteLine("Enter your city:");
-            contact.City = Console.ReadLine()!;
-
+            contact = _dialog.AddDialog(contact);
             _contacts.Add(contact);
             _fileManager.SaveToFile(_contacts);
         }
@@ -34,18 +20,7 @@ namespace Contacts.Servicies
         public void ViewContacts()
         {
             _contacts = _fileManager.GetContacts();
-
-            foreach (Contact contact in _contacts)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"{"Id:",-15} {contact.Id}");
-                Console.WriteLine($"{"Name:", -15} {contact.FirstName} {contact.LastName}");
-                Console.WriteLine($"{"Email:",-15} {contact.Email}");
-                Console.WriteLine($"{"Phone number:",-15} {contact.PhoneNumber}");
-                Console.WriteLine($"{"Address:",-15} {contact.Address}, {contact.ZipCode} {contact.City}");
-                Console.WriteLine();
-            }
-            Console.ReadKey();
+            _dialog.ViewDialog(_contacts);
         }
     }
 }
